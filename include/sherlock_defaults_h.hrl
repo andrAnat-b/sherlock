338d3fc7f931137d68c07c033fc8471a2f5b4ba5
Andrii Burchak <andrii.burchak@betinvest.com>
2023-05-04 07:55:52 +0300

    Fix concurrency

--- include/sherlock_defaults_h.hrl
+++ include/sherlock_defaults_h.hrl
@@ -2,7 +2,8 @@
   -define(SHERLOCK_DEFAULT, true).
 
   -record('DOWN',{ref, type, id, reason}).
-  -define(CTH, 1 bsl 64).
+%%  -define(CTH, 1 bsl 64).
+  -define(CTH, 18446744073709551616).
   -define(DEFAULT_TTL, 5000).
 
 -else.
